3d83657ec6d7e138449fdb59b72ef011cb947d00
Jake <jacobhorne@gmail.com>
2017-09-08 16:47:40 -0400

    added lodash

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 console.log('Beam me up...');
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js')
 
 var res = notes.addNote();
@@ -8,6 +9,11 @@ console.log(res);
 
 var total = notes.add(1, 10);
 console.log(`return = ${total}`);
+
+console.log(_.isString('Andrew'))
+
+nonDuplicateArray = _.uniq(['drew','drew',1,1,1,1,1,3]);
+console.log(nonDuplicateArray);
 
 // var user = os.userInfo();
 // fs.appendFile('greetings.txt', `hello ${user.username}! You are ${notes.age}`);
